69fbdd175c7e217aa87f10cbaa2482da7f93fb87
meonwax <meonwax@meonwax.de>
2016-12-06 16:24:25 +0100

    Click entry to edit expense

--- src/main/app/routes.php
+++ src/main/app/routes.php
@@ -1,4 +1,11 @@
 <?php
+
+abstract class Action {
+  const New = 0;
+  const Edit = 1;
+  const Remove = 2;
+}
+
 // Home
 $app->get('/', function ($request, $response, $args) {
   return $response->withRedirect('/expenses');
@@ -18,17 +25,33 @@ $app->get('/expenses[/]', function ($request, $response, $args) {
 // New
 $app->get('/expenses/new[/]', function ($request, $response, $args) {
   $vars = [
+    'action' => Action::New,
     'categories' => Category::orderBy('id')->get(),
     'persons' => Person::orderBy('id')->get()
   ];
-  return $this->view->render($response, 'new.twig', $vars);
+  return $this->view->render($response, 'expense.twig', $vars);
 })->setName('newExpense');
+
+// Edit
+$app->get('/expenses/edit/{id}', function ($request, $response, $args) {
+  $expense = Expense::find($args['id']);
+  if(!$expense) {
+    return $this->get('notFoundHandler')($request, $response);
+  }
+  $vars = [
+    'action' => Action::Edit,
+    'expense' => $expense,
+    'categories' => Category::orderBy('id')->get(),
+    'persons' => Person::orderBy('id')->get()
+  ];
+  return $this->view->render($response, 'expense.twig', $vars);
+})->setName('editExpense');
 
 // Create
 $app->post('/expenses[/]', function ($request, $response, $args) {
   $data = $request->getParsedBody();
   $expense = new Expense;
-  $expense->date = (new DateTime())->format('Y-m-d');
+  $expense->date = $data['date'];
   $expense->price = $data['price'];
   $expense->description = $data['description'];
   $expense->category_id = $data['category'];
@@ -36,4 +59,19 @@ $app->post('/expenses[/]', function ($request, $response, $args) {
   $expense->save();
   return $response->withRedirect('/expenses');
 })->setName('createExpense');
+
+// Update
+$app->post('/expenses/{id}', function ($request, $response, $args) {
+  $expense = Expense::find($args['id']);
+  if($expense) {
+    $data = $request->getParsedBody();
+    $expense->date = $data['date'];
+    $expense->price = $data['price'];
+    $expense->description = $data['description'];
+    $expense->category_id = $data['category'];
+    $expense->person_id = $data['person'];
+    $expense->save();
+  }
+  return $response->withRedirect('/expenses');
+})->setName('updateExpense');
 ?>
